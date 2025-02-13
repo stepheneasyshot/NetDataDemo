1f6b4aa08d4479b1ca25665f404ba46c617e2d12
stephenzhan <zhanfeng990927@gmail.com>
2025-02-13 12:15:24 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/netdatademo/MainActivity.kt
+++ app/src/main/java/com/example/netdatademo/MainActivity.kt
@@ -5,34 +5,52 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -45,10 +63,9 @@ import com.example.netdatademo.ui.pages.PicturePage
 import com.example.netdatademo.ui.pages.PicturePageKtor
 import com.example.netdatademo.ui.pages.VideoPage
 import com.example.netdatademo.ui.theme.NetDataDemoTheme
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import kotlinx.serialization.Serializable
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
 
@@ -61,9 +78,9 @@ class MainActivity : ComponentActivity() {
 
 //                Scaffold(modifier = Modifier.fillMaxSize(),
 //                    content = { MainContentView(it, mainStateHolder) })
-                Column {
-                    Spacer(modifier = Modifier.height(40.dp))
-                    ListItemAnimateDemo()
+                Column(modifier = Modifier.fillMaxSize()) {
+                    Spacer(modifier = Modifier.height(200.dp))
+                    TransitionAnimation()
                 }
             }
         }
@@ -166,75 +183,46 @@ sealed class Screen(val route: String) {
     data object GithubReposPage : Screen("githubReposPage")
 }
 
-
 @Composable
-fun AnimatedVisibilityDemo() {
-    Column(modifier = Modifier.width(IntrinsicSize.Max)) {
+fun TransitionAnimation() {
+    var boxState by remember { mutableStateOf(BoxState.Collapsed) }
+    val transition = updateTransition(targetState = boxState, label = "box")
 
-        val scope = rememberCoroutineScope()
+    val color by transition.animateColor(
+        label = "color",
+        targetValueByState = { state ->
+            when (state) {
+                BoxState.Collapsed -> Color.Red
+                BoxState.Expanded -> Color.Green
+            }
+        }
+    )
 
-        var state by remember { mutableStateOf(false) }
+    val height by transition.animateDp(
+        label = "height",
+        targetValueByState = { state ->
+            when (state) {
+                BoxState.Collapsed -> 100.dp
+                BoxState.Expanded -> 300.dp
+            }
+        }
+    )
 
-        Text(
-            text = "How are you?",
-            color = Color.White,
-            modifier = Modifier
-                .animateContentSize()
-                .fillMaxWidth(1f)
-                .height(if (state) 160.dp else 80.dp)
-                .background(Color.Red)
-                .clip(RoundedCornerShape(10))
-                .clickable {
-                    state = true
-                    scope.launch {
-                        delay(2000)
-                        state = false
-                    }
-                }
-                .padding(20.dp)
-        )
-
-        Text(
-            text = "I am fine.",
-            color = Color.White,
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .background(Color.Blue)
-                .clip(RoundedCornerShape(10))
-                .padding(20.dp)
-        )
-    }
+    Box(modifier = Modifier
+        .fillMaxWidth(1f)
+        .height(height)
+        .background(color)
+        .clickable {
+            boxState = if (boxState == BoxState.Collapsed) {
+                BoxState.Expanded
+            } else {
+                BoxState.Collapsed
+            }
+        })
 }
 
-@Composable
-fun ListItemAnimateDemo() {
-    val listState = remember { mutableStateListOf<ListItem>() }
-
-    LaunchedEffect(Unit) {
-        repeat(10) {
-            listState.add(ListItem(it, "Item $it"))
-            delay(1000)
-        }
-        delay(1000)
-        listState.removeAt(5)
-    }
-
-    LazyColumn(modifier = Modifier.fillMaxSize()) {
-        items(listState, key = { it.id }) { item ->
-            Text(
-                text = item.title,
-                color = Color.White,
-                modifier = Modifier
-                    .fillMaxWidth(1f)
-                    .animateItem()
-                    .background(Color.Blue)
-                    .clip(RoundedCornerShape(10))
-            )
-        }
-    }
+enum class BoxState {
+    Collapsed,
+    Expanded
 }
 
-data class ListItem(
-    val id: Int,
-    val title: String,
-)
